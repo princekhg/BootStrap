eff5f7c9eefc4f753fbe419d2b374f65d0f70a2b
Shubham Prince <princekhg1@gmail.com>
2022-03-03 23:20:03 +0530

    This is the first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+$(window).scroll(function() {
+  $('navbar').toggleClass('scrolled',$(this).scrollTop()>200);
+});
+
+
+
+
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+};
